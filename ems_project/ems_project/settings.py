3c0bb141250f3e57881397688bac24ffe737958e
Your Name <your.email@gmail.com>
2026-01-13 16:09:16 +0530

    Fix SECRET_KEY environment variable

--- ems_project/ems_project/settings.py
+++ ems_project/ems_project/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*61)$mej-j#iu2gaduz@l$z7a3b3c7e&x%07f*7^=(s6mtuyde'
+import os
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-*61)$mej-j#iu2gaduz@l$z7a3b3c7e&x%07f*7^=(s6mtuyde')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-import os
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = ['*']
